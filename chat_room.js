57f68e88e86f6d307ad52b8112b7003c1e22c43f
Hecker51 <111092624+Hecker51@users.noreply.github.com>
2023-01-12 16:38:00 -0300

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,20 @@
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCNBQYPTAK-8vPcUSk3n_OQZi7UcZogaD0",
+  
+    authDomain: "jhgfd-46b27.firebaseapp.com",
+  
+    databaseURL: "https://jhgfd-46b27-default-rtdb.firebaseio.com",
+  
+    projectId: "jhgfd-46b27",
+  
+    storageBucket: "jhgfd-46b27.appspot.com",
+  
+    messagingSenderId: "98191139639",
+  
+    appId: "1:98191139639:web:6e62c262a2a02bfa9f30b2"
+  
+  };
+  
